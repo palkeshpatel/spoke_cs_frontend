9932253af11e84b70f150e36a889d7751e18add4
Palkesh Patel <patel.palkesh@gmail.com>
2026-08-03 23:41:42 +0530

    Fix edit mode resetting when opening Customization Dialog

--- src/views/OrderNew.tsx
+++ src/views/OrderNew.tsx
@@ -1878,7 +1878,6 @@ export default function OrderNew() {
           if (!isOpen) {
             setCustomizationDialogOpen(false);
             setActiveCustomizationTarget(null);
-            setEditingItemIndex(null);
           }
         }}
         selectedOptions={
